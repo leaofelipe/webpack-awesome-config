e502c5f0fbacdc69ec4f9c2d6ca9b23495c4b598
Felipe Leão <felipe@felipeleao.com>
2019-01-21 17:33:40 -0200

    Add CSS loader

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
 console.log('It Works')
+import "./style/main.css"
--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,20 @@
 const webpack = require("webpack")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
 const webpackMerge = require("webpack-merge")
+const modeConfig = env => require(`./build-utils/webpack.${env}`)(env);
+const presetConfig = require("./build-utils/loadPresets");
 
-module.exports = ({ mode, presets } = { mode: "production", presets: [] }) => {
-  return webpackMerge({
-    mode,
-    output: {filename: "bundle.js"},
-    plugins: [
-      new HtmlWebpackPlugin(),
-      new webpack.ProgressPlugin()
-    ]
-  })
+module.exports = ({ mode, presets } = { mode: "development", presets: [] }) => {
+  return webpackMerge(
+    {
+      mode,
+      output: {filename: "bundle.js"},
+      plugins: [
+        new HtmlWebpackPlugin(),
+        new webpack.ProgressPlugin()
+      ]
+    },
+    modeConfig(mode),
+    presetConfig({ mode, presets })
+  )
 }
-
-/* const webpack = require("webpack"); */
-/* const HtmlWebpackPlugin = require("html-webpack-plugin"); */
-/* const webpackMerge = require("webpack-merge"); */
-
-/* const modeConfig = env => require(`./build-utils/webpack.${env}`)(env); */
-/* const presetConfig = require("./build-utils/loadPresets"); */
-
-/* module.exports = ({ mode, presets } = { mode: "production", presets: [] }) => { */
-/*   return webpackMerge( */
-/*     { */
-/*       mode, */
-/*       module: { */
-/*         rules: [ */
-/*           { */
-/*             test: /\.jpe?g$/, */
-/*             use: [ */
-/*               { */
-/*                 loader: "url-loader", */
-/*                 options: { */
-/*                   limit: 5000 */
-/*                 } */
-/*               } */
-/*             ] */
-/*           } */
-/*         ] */
-/*       }, */
-/*       output: { */
-/*         filename: "bundle.js" */
-/*       }, */
-/*       plugins: [new HtmlWebpackPlugin(), new webpack.ProgressPlugin()] */
-/*     }, */
-/*     modeConfig(mode), */
-/*     presetConfig({ mode, presets }) */
-/*   ); */
-/* }; */
