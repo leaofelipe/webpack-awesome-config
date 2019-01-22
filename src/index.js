3662e5d92f81765e2e7f4a22df7ad7b57062d6b8
Felipe Leão <felipe@felipeleao.com>
2019-01-22 11:42:57 -0200

    Remove dependencies

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 console.log('It Works')
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Start from './components/start'
 import './style/main.scss'
 
 const App = (
   <div>
     React APP
-    <Start />
   </div>
 )
 
