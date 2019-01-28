e5cf121820ba0d9c042dbacc777a9f1bcb30ec93
Felipe Leão <felipe@felipeleao.com>
2019-01-28 11:16:05 -0200

    Create Lints

--- src/index.js
+++ src/index.js
@@ -2,10 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './style/main.scss'
 
-const App = (
-  <div>
-    React APP
-  </div>
-)
+const App = <div>React APP</div>
 
 ReactDOM.render(App, document.getElementById('app'))
