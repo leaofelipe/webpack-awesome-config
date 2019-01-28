2facd128cd87b8bd8bdef7185032524f48182e19
Felipe Leão <felipe@felipeleao.com>
2019-01-28 17:55:52 -0200

    Add Lint

--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
 import './style/main.scss'
-
-const AB = ({ teste }) => {
-  return <div>{teste}</div>
-}
-
-AB.propTypes = {
-  teste: PropTypes.bool
-}
 
 const App = <div>React APP</div>
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,34 @@
 const webpack = require('webpack')
 const webpackMerge = require('webpack-merge')
-const modeConfig = env => require(`./build-utils/webpack.${env}`)(env)
+const modeConfig = env => require(`./build-utils/webpack.${env}`)(env) // eslint-disable-line global-require
 const presetConfig = require('./build-utils/loadPresets')
 
-module.exports = ({ mode, presets } = { mode: 'development', presets: [] }) => {
-  return webpackMerge({
-    mode,
-    entry: ['@babel/polyfill', './src/index.js'],
-    output: { filename: 'bundle.js' },
-    module: {
-      rules: [{
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        }
-      }, {
-        test: /\.(eot|ttf|woff|woff2)$/,
-        loader: 'url-loader',
-        options: {
-          name: 'style/[name].[ext]'
-        }
-      }]
+module.exports = ({ mode, presets } = { mode: 'development', presets: [] }) =>
+  webpackMerge(
+    {
+      mode,
+      entry: ['@babel/polyfill', './src/index.js'],
+      output: { filename: 'bundle.js' },
+      module: {
+        rules: [
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader'
+            }
+          },
+          {
+            test: /\.(eot|ttf|woff|woff2)$/,
+            loader: 'url-loader',
+            options: {
+              name: 'style/[name].[ext]'
+            }
+          }
+        ]
+      },
+      plugins: [new webpack.ProgressPlugin()]
     },
-    plugins: [new webpack.ProgressPlugin()]
-  },
-  modeConfig(mode),
-  presetConfig({ mode, presets })
+    modeConfig(mode),
+    presetConfig({ mode, presets })
   )
-}
